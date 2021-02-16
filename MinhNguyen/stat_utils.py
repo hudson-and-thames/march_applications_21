01f574a07f40c5983037f7bb51b8d44dfbce551d
Minh Nguyen <mdn0420@gmail.com>
2021-02-16 11:41:14 -0800

    Add comments and other util methods

--- MinhNguyen/stat_utils.py
+++ MinhNguyen/stat_utils.py
@@ -1,25 +1,25 @@
-from statsmodels.distributions.empirical_distribution import ECDF
-import scipy.special
 import numpy as np
+import scipy.special
 
 class SpearmanExt():
-    """
-    This is an implementation of a generalized Spearman's rho for higher dimensionality as described in 
+    """This is an implementation of a generalized Spearman's rho for higher dimensionality as described in 
     Schmid, F., Schmidt, R., 2007. Multivariate extensions of Spearman’s rho and related statistics.
-
-    data is quantile data expected to be a Numpy array of shape (i,j) where i is the number of dimensions and j is
-                number of samples
     """
     def __init__(self, data):
+        """Constructor
+
+        :param data: (pd.DataFrame): quantile data of shape (i,j) where i is the number of dimensions and j is number of samples
+        """
+        # Pre calculate values used across estimators
         self.d = d = len(data)
         self.n = len(data[0])
         self._hd = (d+1) / (2**d - d - 1)
-        # Multplier for r1 and
         self._mult = (2**4)/self.n
         self.u = data
 
     @property
     def r1(self):
+        """Calculates and returns the first estimator"""
         inner = 1 - self.u
         inner_prod = np.prod(inner, axis=0)
         outer = inner_prod.sum() * self._mult - 1
@@ -27,12 +27,14 @@ class SpearmanExt():
 
     @property
     def r2(self):
+        """Calculates and returns the second estimator"""
         inner_prod = np.prod(self.u, axis=0)
         outer = inner_prod.sum() * self._mult - 1
         return self._hd * outer
 
     @property
     def r3(self):
+        """Calculates and returns the third estimator"""
         right = 0
         u = self.u
         for l in range(1, self.d):
