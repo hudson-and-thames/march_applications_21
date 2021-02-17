fixed wrong axis calculation in geometric sum

FranzKrekeler/vinecopulaslab/pairselection/extended.py
@@ -3,37 +3,53 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import scipy.special
+from typing import List
 from statsmodels.distributions.empirical_distribution import ECDF
 from .base import SelectionBase
 
 
 class ExtendedSelection(SelectionBase):
-    def _transform_df(self, df_returns):
+    def _transform_df(self, df_returns: pd.DataFrame) -> pd.DataFrame:
+        """
+        Calculates the ECDF for each stock
+        """
         ecdf_df = df_returns.apply(lambda x: ECDF(x)(x), axis=0)
         return ecdf_df
 
-    def _get_pair_with_highest_corr_sum(self, group):
+    def _get_partner_for_target_stock(self, group) -> List[str]:
+        """
+        Helper function for df.groupby("TARTGET_STOCK").apply(...)
+         :param group: (group) The group of 50 most correlated stocks
+        :return: (List[str]) returns a list of highest correlated quadruple
+        """
         target_stock = group.name
-        combinations = group.STOCK_PAIR.tolist()
-        stock_selection = [target_stock] + combinations
-        df_subset = self.ecdf_df[stock_selection].copy()
-        num_of_stocks = len(stock_selection)
+        partner_stocks = group.STOCK_PAIR.tolist()
+        all_stocks = [target_stock] + partner_stocks
+        # We create a subset of our ecdf dataframe to increase lookup speed.
+        df_subset = self.ecdf_df[all_stocks].copy()
+        num_of_stocks = len(all_stocks)
+        # We turn our partner stocks into numerical indices so we can use them directly for indexing
         possible_pairs_for_target_stock = np.arange(num_of_stocks)[1:]  # exclude the target stock
-        all_possible_combinations = np.array(list((0,) + comb for comb in itertools.combinations(possible_pairs_for_target_stock, 3)))
-        all_possible_df = df_subset.values[:, all_possible_combinations]
-        n, _, d = all_possible_df.shape
+        partner_combinations = itertools.combinations(possible_pairs_for_target_stock, 3)
+        # Let's add the target stock
+        combinations_quadruples = np.array(list((0,) + comb for comb in partner_combinations))
+        #We can now use our list of possible quadruples as an index
+        df_all_quadruples = df_subset.values[:, combinations_quadruples]
+        # Now we can get closer to a vectorized calculation 
+        n, _, d = df_all_quadruples.shape
         hd = (d + 1)/(2**d-d-1)
-        ecdf_df_product = np.product(all_possible_df, axis=-1)
+        ecdf_df_product = np.product(df_all_quadruples, axis=-1)
         est1 = hd * (-1 + (2**d / n) * (1-ecdf_df_product).sum(axis=0))
         est2 = hd * (-1 + (2**d / n) * ecdf_df_product.sum(axis=0))
         idx = np.array([(k, l) for l in range(0, d) for k in range(0, l)])
-        est3 = -3 + (12 / (n*scipy.special.comb(n, 2, exact=True))) * ((1 - all_possible_df[:, :, idx[:, 0]])*(1-all_possible_df[:, :, idx[:, 1]])).sum(axis=(0, 2))
-        res = (est1 + est2 + est3)/3
-        max_index = np.argmax(res)
-        return df_subset.columns[list(all_possible_combinations[max_index])].tolist()
+        est3 = -3 + (12 / (n*scipy.special.comb(n, 2, exact=True))) * ((1 - df_all_quadruples[:, :, idx[:, 0]])*(1-df_all_quadruples[:, :, idx[:, 1]])).sum(axis=(0, 2))
+        quadruples_scores = (est1 + est2 + est3)/3
+        #The quadruple scores have the shape of (19600,1) now
+        max_index = np.argmax(quadruples_scores)
+        return df_subset.columns[list(combinations_quadruples[max_index])].tolist()
 
     def _get_quadruples(self, df_corr_top50):
-        qf = df_corr_top50.groupby('TARGET_STOCK').apply(self._get_pair_with_highest_corr_sum)
+        qf = df_corr_top50.groupby('TARGET_STOCK').apply(self._get_partner_for_target_stock)
         return qf
 
     def select_pairs(self, df):

FranzKrekeler/vinecopulaslab/pairselection/geometric.py
@@ -8,22 +8,25 @@ import numpy as np
 class GeometricSelection(SelectionBase):
     def _get_pair_with_highest_corr_sum(self, group):
         target_stock = group.name
-        combinations = group.STOCK_PAIR.tolist()
-        stock_selection = [target_stock] + combinations
-        df_subset = self.ranked_df[stock_selection].copy()
-        num_of_stocks = len(stock_selection)
-        possible_pairs_for_target_stock = np.arange(num_of_stocks)[1:]  ## exclude the target stock
-        all_possible_combinations = np.array(list((0,) + comb for comb in itertools.combinations(possible_pairs_for_target_stock, 3)))
-        all_possible_df = df_subset.values[:,all_possible_combinations]
-        # TO FIX
-        all_possible_df = all_possible_df.reshape(len(all_possible_combinations), -1,4)
+        partner_stocks = group.STOCK_PAIR.tolist()
+        all_stocks = [target_stock] + partner_stocks
+        # We create a subset of our ecdf dataframe to increase lookup speed.
+        df_subset = self.ranked_df[all_stocks].copy()
+        num_of_stocks = len(all_stocks)
+        # We turn our partner stocks into numerical indices so we can use them directly for indexing
+        possible_pairs_for_target_stock = np.arange(num_of_stocks)[1:]  # exclude the target stock
+        partner_combinations = itertools.combinations(possible_pairs_for_target_stock, 3)
+        # Let's add the target stock
+        combinations_quadruples = np.array(list((0,) + comb for comb in partner_combinations))
+        #We can now use our list of possible quadruples as an index
+        df_all_quadruples = df_subset.values[:, combinations_quadruples]
         line = np.ones(4)
-        pp = np.dot(all_possible_df, line)/np.linalg.norm(line)
-        pn = np.linalg.norm(all_possible_df, axis=-1)
+        pp = (np.einsum("ijk,k->ji", df_all_quadruples, line)/np.linalg.norm(line))
+        pn = np.sqrt(np.einsum('ijk,ijk->ji', df_all_quadruples, df_all_quadruples))
         res = np.sqrt(pn**2 - pp**2).sum(axis=1)
-
-        max_index = np.argmin(res)
-        return df_subset.columns[list(all_possible_combinations[max_index])].tolist()
+        min_index = np.argmin(res)
+        partners = df_subset.columns[list(combinations_quadruples[min_index])].tolist()
+        return partners
 
 
     def distance_to_line(self, line, pts):
@@ -41,6 +44,38 @@ class GeometricSelection(SelectionBase):
 
     def convert_pairs_series_to_list(self, pairs):
         return np.concatenate((np.array([pairs.index.tolist()]).T, pairs.tolist()), axis=1)
+
+    def _select_pairs_slow(self, df: pd.DataFrame):
+        df_returns = self._returns(df)
+        self.df_corr = self._ranked_correlations(df)
+        self.ranked_df = df_returns.rank(pct=True)
+        df_corr_top50 = self._top_50_correlations(self.df_corr)
+        for name, group in df_corr_top50.groupby('TARGET_STOCK'):
+            return self._get_pair_with_highest_corr_slow(name,group)
+        return 
+
+    def _get_pair_with_highest_corr_slow(self, name,group):
+        target_stock = name
+        partner_stocks = group.STOCK_PAIR.tolist()
+        all_stocks = [target_stock] + partner_stocks
+        df_subset = self.ranked_df[all_stocks].copy()
+        num_of_stocks = len(all_stocks)
+        possible_pairs_for_target_stock = np.arange(num_of_stocks)[1:]  ## exclude the target stock
+        partner_combinations = itertools.combinations(possible_pairs_for_target_stock, 3)
+        # Let's add the target stock
+        combinations_quadruples = np.array(list((0,) + comb for comb in partner_combinations))
+        #We can now use our list of possible quadruples as an index      
+        scores = []
+        line = np.ones(4)
+        for combination in combinations_quadruples:
+            df_combination = df_subset.iloc[:,combination]
+            pp = np.dot(df_combination, line)/np.linalg.norm(line)
+            pn = np.linalg.norm(df_combination, axis=-1)
+            res = np.sqrt(pn**2 - pp**2).sum()
+            scores.append(res)
+        min_index = np.argmin(scores)
+        print(np.min(scores))
+        return df_subset.columns[list(combinations_quadruples[min_index])].tolist()
 
     def select_pairs(self, df: pd.DataFrame):
         df_returns = self._returns(df)

FranzKrekeler/vinecopulaslab/pairselection/traditional.py
@@ -6,7 +6,15 @@ import numpy as np
 
 
 class TraditionalSelection(SelectionBase):
-    def _get_pair_with_highest_corr_sum(self, group: pd.DataFrame):
+    """
+    This class implements the traditional approach for pair selection. Mentioned section 3.1
+    of the paper "Statistical arbitrage with vine copulas" https://www.econstor.eu/bitstream/10419/147450/1/870932616.pdf
+    """
+    def _get_highest_pairs(self, group: pd.DataFrame):
+        """
+        internal helper function for apply in Pandas
+        :param group: (pd.DataFrame) this the result of .groupBy...
+        """
         target_stock = group.name
         combinations = group.STOCK_PAIR.tolist()
         # Create a subset dataframe of all top 50 correlated stocks + target stock.
@@ -37,16 +45,14 @@ class TraditionalSelection(SelectionBase):
         # Afterwards we return the maximum index for the sums
         max_index = np.argmax((np.expand_dims(one_hot, axis=2) * np.expand_dims(df_subset, axis=0) * np.expand_dims(one_hot, axis=1)).sum(axis=(1, 2)))
         # Finally convert the index to the list of stocks and return the column names
-        return df_subset.columns[list(all_possible_combinations[max_index])].tolist()
-
-    def _get_quadruples(self, df_corr_top50):
-        qf = df_corr_top50.groupby('TARGET_STOCK').apply(self._get_pair_with_highest_corr_sum)
-        return qf
-
-    def convert_pairs_series_to_list(self, pairs):
-        return np.concatenate((np.array([pairs.index.tolist()]).T, pairs.tolist()), axis=1)
+        return [target_stock] + df_subset.columns[list(all_possible_combinations[max_index])].tolist()
 
     def select_pairs(self, df: pd.DataFrame):
+        """
+        main function to return quadruples of correlated stock (spearman)
+        :return: (pd.DataFrame)
+        """
         self.df_corr = self._ranked_correlations(df)
         df_corr_top50 = self._top_50_correlations(self.df_corr)
-        return self._get_quadruples(df_corr_top50)
+        quadruples = df_corr_top50.groupby('TARGET_STOCK').apply(self._get_highest_pairs)
+        return quadruples