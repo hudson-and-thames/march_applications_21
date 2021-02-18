ad1fb498d6fd2dddad5d1766fbc60a138fea42b4
Franz <fkrekeler@gmail.com>
2021-02-18 21:51:42 +0100

    added cached files to skip downloads

--- FranzKrekeler/vinecopulaslab/partnerselection/geometric.py
+++ FranzKrekeler/vinecopulaslab/partnerselection/geometric.py
@@ -6,16 +6,20 @@ import numpy as np
 
 
 class GeometricSelection(SelectionBase):
+    """
+    This class implements the geometric approach for partner selection. Mentioned section 3.1
+    of the paper "Statistical arbitrage with vine copulas" https://www.econstor.eu/bitstream/10419/147450/1/870932616.pdf
+    """
     def _find_partners_for_target_stock(self, group):
         """
         Helper function for df.groupby("TARTGET_STOCK").apply(...)
          :param group: (group) The group of 50 most correlated stocks
-        :return: (List[str]) returns a list of highest correlated quadruple
+        :return: (List[str]) returns a list of highest scored quadruple
         """
         target_stock = group.name
         partner_stocks = group.STOCK_PAIR.tolist()
         all_stocks = [target_stock] + partner_stocks
-        # We create a subset of our ecdf dataframe to increase lookup speed.
+        # We create a subset of our rank transformed dataframe to increase lookup speed.
         df_subset = self.ranked_df[all_stocks].copy()
         num_of_stocks = len(all_stocks)
         # We turn our partner stocks into numerical indices so we can use them directly for indexing
@@ -25,11 +29,15 @@ class GeometricSelection(SelectionBase):
         combinations_quadruples = np.array(list((0,) + comb for comb in partner_combinations))
         # We can now use our list of possible quadruples as an index
         df_all_quadruples = df_subset.values[:, combinations_quadruples]
-        line = np.ones(4)
+        # Now we will create a diagonal for our distance calculation.
+        # Please reffer to the paper
+        n,d = self.ranked_df.shape
+        line = np.ones(d)
+        #this extends the distance method for all 19600 combinations
         pp = (np.einsum("ijk,k->ji", df_all_quadruples, line)/np.linalg.norm(line))
         pn = np.sqrt(np.einsum('ijk,ijk->ji', df_all_quadruples, df_all_quadruples))
-        res = np.sqrt(pn**2 - pp**2).sum(axis=1)
-        min_index = np.argmin(res)
+        distance_scores = np.sqrt(pn**2 - pp**2).sum(axis=1)
+        min_index = np.argmin(distance_scores)
         partners = df_subset.columns[list(combinations_quadruples[min_index])].tolist()
         return partners
 
@@ -37,15 +45,19 @@ class GeometricSelection(SelectionBase):
     def distance_to_line(line, pts):
         """
         original helper function
+        :param line: the line endpoint assuming it starts at point zero. For example np.array([1,1,1]) for a 3d line
+        :param pts: the points to measure the distance to the line
+        :return: float np.array with distances
         """
-        dp = np.dot(pts, line)
+        dp = np.dot(pts,line)
         pp = dp/np.linalg.norm(line)
-        pn = np.linalg.norm(pts, axis=-1)
+        pn = np.linalg.norm(pts, axis=1)
+        print(np.sqrt((pn**2) - (pp**2)))
         return np.sqrt(pn**2 - pp**2)
 
     def find_partners(self, df: pd.DataFrame):
         """
-        main function to return quadruples of correlated stock using a geometric approach for more read the paper that is linked Readme
+        main function to return quadruples of correlated stock using the geometric approach of the paper "Statistical arbitrage with vine copulas" section 3.1
         :return: (pd.DataFrame)
         """
         df_returns = self._returns(df)
