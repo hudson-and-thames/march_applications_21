10609df9675f28f9f205430481ea2aa223ffede7
JuanKo96 <juanko@yonsei.ac.kr>
2021-02-19 23:44:06 +0900

    typo
    typo

--- Joohwan Ko/test.py
+++ Joohwan Ko/test.py
@@ -2,7 +2,6 @@ import unittest
 from data_handler import ImportData
 
 
-# TestCase를 작성
 class CustomTests(unittest.TestCase):
 
     def test_data_handler_sp500(self):
@@ -12,6 +11,5 @@ class CustomTests(unittest.TestCase):
         self.assertEqual(len(ImportData.download_data('GOOG','2020-01-01','2020-01-10')),7)
 
 
-# unittest를 실행
 if __name__ == '__main__':
     unittest.main()
