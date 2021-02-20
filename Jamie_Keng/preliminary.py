65dc0c5ac40520bf5649aa56f426df3c8cc8d8da
jamiekeng1016 <37807858+jamiekeng1016@users.noreply.github.com>
2021-02-19 17:53:30 -0800

    Update preliminary.py

--- Jamie_Keng/preliminary.py
+++ Jamie_Keng/preliminary.py
@@ -1,17 +1,13 @@
 import numpy as np
 import yfinance as yf
 import yahoo_fin.stock_info as ys
-
 """
 This class contains preliminary steps before data analysis.
-
 1. Download historical price data of S&P500 constituents.
 2. NaN elements removal(reference: Hansen Pei). 
 3. Return calculation(reference: Hansen Pei).
-
 @author: Jamie Keng
 """
-
 
 class Preliminary:
 
@@ -20,25 +16,11 @@ class Preliminary:
         """
         Parameters
         ----------
-        start_date : str
-            Download start date string (YYYY-MM-DD) or _datetime.
+        start_date -- Download start date string (YYYY-MM-DD) or _datetime.
+        end_date --Download end date string (YYYY-MM-DD) or _datetime.
 
-        end_date : str
-            Download end date string (YYYY-MM-DD) or _datetime.
-
-        interval : str, OPTIONAL
-            Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-            default value is '1d'
-
-        num : int
-            Number of randomly picked stocks out of S&P500 index.
-            default value is 100.
-
-        Returns
-        -------
-        hist_data : pandas dataframe
-            The downloaded historical price dataframe.
-
+        interval -- Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        num -- Number of randomly picked stocks out of S&P500 index.
         """
         # Get the S&P 500 stocks tickers.
         tickers_sp500 = ys.tickers_sp500()
@@ -49,33 +31,21 @@ class Preliminary:
         start_date = start_date
         end_date = end_date
         interval = interval
-
         hist_data = yf.download(tickers,
                                 start=start_date,
                                 end=end_date,
                                 interval=interval,
                                 group_by='column')['Close']
-
         return hist_data
 
     @staticmethod
     def remove_nuns(df, threshold=100):
-        """
-        Remove tickers with nuns in value over a threshold.
+        """Remove tickers with nuns in value over a threshold.
 
         Parameters
         ----------
-        df : pandas dataframe
-            Price time series dataframe
-
-        threshold: int, OPTIONAL
-            The number of null values allowed
-            Default is 100
-
-        Returns
-        -------
-        df : pandas dataframe
-            Updated price time series without any nuns
+        df : Price time series dataframe
+        threshold: The number of null values allowed
         """
         null_sum_each_ticker = df.isnull().sum()
         tickers_under_threshold = \
@@ -86,18 +56,11 @@ class Preliminary:
 
     @staticmethod
     def get_returns_data(hist_data):
-        """
-        Calculate the return of historical price data.
+        """Calculate the return of historical price data.
 
         Parameters
         ----------
-        hist_data : pandas dataframe
-            The price data
-
-        Returns
-        -------
-        returns_data : pandas dataframe
-            The requested returns data.
+        hist_data -- The price data
         """
         returns_data = hist_data.pct_change()
         returns_data = returns_data.iloc[1:]
@@ -106,19 +69,11 @@ class Preliminary:
 
     @staticmethod
     def stock_index(df):
-        """
-        Stock Index Correspondence
+        """Stock Index Correspondence
 
         Parameters
         ----------
-        df : pandas dataframe
-            historical stock price dataset/return dataset
-
-        Returns
-        -------
-        stock_ind_dict : dictionary
-            keys : stock indexes in the dataframe
-            values : stock tickers
+        df : historical stock price dataset/return dataset
         """
         indexes = list(enumerate(df.columns))
 
